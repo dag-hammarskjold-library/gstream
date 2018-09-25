75762e2e21ae0fb716285ea8e365943469600280
Aaron Helton <mariusagricola@gmail.com>
2018-09-25 14:01:20 -0400

    try closing open connections..

--- gstream/gstreamer2.py
+++ gstream/gstreamer2.py
@@ -9,6 +9,7 @@ from lxml.html.soupparser import fromstring
 import sys, logging, socket
 import urllib.parse, urllib.request
 import datetime, os, json, hashlib, boto3, botocore
+import contextlib
 
 def download_to_s3(bucket,filename,body):
     logging.info("%s - Saving %s/%s" % (datetime.datetime.now().__str__(),bucket,filename) )
@@ -48,18 +49,18 @@ def get_symbol_list(ds,query_date):
     url = Config.GDOC_HOST + urllib.parse.urlencode(Config.STATIC_PARAMS) + '&' + urllib.parse.urlencode(Config.DYNAMIC_PARAMS)
     # unclear if this should be enclosed in a try/except statement...
     # or what we would do about an issue like a timeout
-    zipfile = ZipFile(BytesIO(urllib.request.urlopen(url, timeout=60).read()))
-    if 'export.txt' in zipfile.namelist():
-        # we're really only interested here in the list of symbols with files issued
-        # on the specified date. This file could be empty...
-        metadata = zipfile.open('export.txt')
-        results = json.load(metadata)
-        for result in results:
-            this_symbol = result['symbol1']
-            symbol_set = (ds,query_date,this_symbol)
-            if symbol_set not in return_symbols:
-                return_symbols.append(symbol_set)
-
+    with contextlib.closing(urllib.request.urlopen(url, timeout=60)) as x:
+        zipfile = ZipFile(BytesIO(x.read()))
+        if 'export.txt' in zipfile.namelist():
+            # we're really only interested here in the list of symbols with files issued
+            # on the specified date. This file could be empty...
+            metadata = zipfile.open('export.txt')
+            results = json.load(metadata)
+            for result in results:
+                this_symbol = result['symbol1']
+                symbol_set = (ds,query_date,this_symbol)
+                if symbol_set not in return_symbols:
+                    return_symbols.append(symbol_set)
     logging.info('%s - Found %s symbols from %s with files re/issued on %s' % (datetime.datetime.now().__str__(),len(return_symbols),ds,query_date) )
     return return_symbols     
 
@@ -110,32 +111,31 @@ for ds in symbol_list:
         Config.DYNAMIC_PARAMS['DateTo'] = this_date
         Config.DYNAMIC_PARAMS['Symbol'] = this_symbol
         url = Config.GDOC_HOST + urllib.parse.urlencode(Config.STATIC_PARAMS) + '&' + urllib.parse.urlencode(Config.DYNAMIC_PARAMS_NO_SYMBOl) + "&Symbol=%s" % this_symbol
-        print(url)
-        zipfile = ZipFile(BytesIO(urllib.request.urlopen(url, timeout=60).read()))
-        if 'export.txt' in zipfile.namelist():
-            # Now we get all of the files associated with the symbol in question
-            metadata = zipfile.open('export.txt')
-            results = json.load(metadata)
-            logging.info('%s - Found %s files for %s on %s.' % (datetime.datetime.now().__str__(),len(results),this_symbol,this_date))
-            for result in results:
-                matching_file = [n for n in zipfile.namelist() if this_odsno + '.pdf' in n][0]
-                checksum = hashlib.md5(zipfile.open(matching_file).read()).hexdigest()
-                body = zipfile.open(matching_file).read()
-                # try saving it to S3; the method will skip duplicates
-                download_to_s3(Config.BUCKET,checksum,body)
-                # next we will try to write the data to DDB
-                result['checksum'] = checksum
-                result['dutyStation'] = this_ds
-                # first we need to try resolving the symbol
-                undl_link = resolve(this_symbol)
-                #make sure there are no empty key values
-                for k in result:
-                    result[k] = str(result[k])
-                    if ('Date' in k or k == 'embargo'):
-                        # we have a date
-                        d,m,y = result[k].split('/')
-                        this_d = int(datetime.date(int(y),int(m),int(d)).__str__().replace('-',''))
-                        result[k] = this_d
-                    if len(str(result[k])) == 0:
-                        result[k] = "_"
-            
+        with contextlib.closing(urllib.request.urlopen(url, timeout=60)) as x:
+            zipfile = ZipFile(BytesIO(x.read()))
+            if 'export.txt' in zipfile.namelist():
+                # Now we get all of the files associated with the symbol in question
+                metadata = zipfile.open('export.txt')
+                results = json.load(metadata)
+                logging.info('%s - Found %s files for %s on %s.' % (datetime.datetime.now().__str__(),len(results),this_symbol,this_date))
+                for result in results:
+                    matching_file = [n for n in zipfile.namelist() if this_odsno + '.pdf' in n][0]
+                    checksum = hashlib.md5(zipfile.open(matching_file).read()).hexdigest()
+                    body = zipfile.open(matching_file).read()
+                    # try saving it to S3; the method will skip duplicates
+                    download_to_s3(Config.BUCKET,checksum,body)
+                    # next we will try to write the data to DDB
+                    result['checksum'] = checksum
+                    result['dutyStation'] = this_ds
+                    # first we need to try resolving the symbol
+                    undl_link = resolve(this_symbol)
+                    #make sure there are no empty key values
+                    for k in result:
+                        result[k] = str(result[k])
+                        if ('Date' in k or k == 'embargo'):
+                            # we have a date
+                            d,m,y = result[k].split('/')
+                            this_d = int(datetime.date(int(y),int(m),int(d)).__str__().replace('-',''))
+                            result[k] = this_d
+                        if len(str(result[k])) == 0:
+                            result[k] = "_"
