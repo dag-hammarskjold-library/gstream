0420c62dfe76cfe051aa127a7d6ab5db5a2e2eb9
Aaron Helton <mariusagricola@gmail.com>
2018-09-26 14:37:33 -0400

    remove log etc

--- gstream/gstreamer2.py
+++ gstream/gstreamer2.py
@@ -9,7 +9,7 @@ from lxml.html.soupparser import fromstring
 import sys, logging, socket
 import urllib.parse, urllib.request
 import datetime, os, json, hashlib, boto3, botocore
-import contextlib
+import contextlib, gc, time
 
 def download_to_s3(bucket,filename,body):
     logging.info("%s - Saving %s/%s" % (datetime.datetime.now().__str__(),bucket,filename) )
@@ -88,6 +88,9 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
+static_params = Config.STATIC_PARAMS
+dynamic_params = Config.DYNAMIC_PARAMS
+
 # check argv to see if I passed a date
 query_date = Config.DYNAMIC_PARAMS['DateFrom']
 try:
@@ -104,14 +107,15 @@ for ds in Config.DUTY_STATIONS:
 # Now we can try to get the files and metadata. This should minimize the chance
 # of timeouts that can occur when lots of files are issued in a day.
 for ds in symbol_list:
-    Config.STATIC_PARAMS['DownloadFiles'] = 'Y'
+    static_params['DownloadFiles'] = 'Y'
+    print("Processing %s" % ds)
     for (this_ds,this_date,this_symbol) in tqdm(symbol_list[ds]):
-        Config.DYNAMIC_PARAMS['DutyStation'] = this_ds
-        Config.DYNAMIC_PARAMS['DateFrom'] = this_date
-        Config.DYNAMIC_PARAMS['DateTo'] = this_date
-        Config.DYNAMIC_PARAMS['Symbol'] = this_symbol
-        url = Config.GDOC_HOST + urllib.parse.urlencode(Config.STATIC_PARAMS) + '&' + urllib.parse.urlencode(Config.DYNAMIC_PARAMS_NO_SYMBOl) + "&Symbol=%s" % this_symbol
-        with contextlib.closing(urllib.request.urlopen(url, timeout=60)) as x:
+        dynamic_params['DutyStation'] = this_ds
+        dynamic_params['DateFrom'] = this_date
+        dynamic_params['DateTo'] = this_date
+        this_url = Config.GDOC_HOST + urllib.parse.urlencode(static_params) + '&' + urllib.parse.urlencode(dynamic_params) + "&Symbol=%s" % this_symbol
+        logging.debug(this_url)
+        with contextlib.closing(urllib.request.urlopen(this_url, timeout=60)) as x:
             zipfile = ZipFile(BytesIO(x.read()))
             if 'export.txt' in zipfile.namelist():
                 # Now we get all of the files associated with the symbol in question
@@ -119,6 +123,7 @@ for ds in symbol_list:
                 results = json.load(metadata)
                 logging.info('%s - Found %s files for %s on %s.' % (datetime.datetime.now().__str__(),len(results),this_symbol,this_date))
                 for result in results:
+                    this_odsno = str(result['odsNo'])
                     matching_file = [n for n in zipfile.namelist() if this_odsno + '.pdf' in n][0]
                     checksum = hashlib.md5(zipfile.open(matching_file).read()).hexdigest()
                     body = zipfile.open(matching_file).read()
