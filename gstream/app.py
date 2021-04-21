d90b3b28e308bf9663a249dd4b10fe7f737ca853
Aaron Helton <mariusagricola@gmail.com>
2021-04-21 17:34:12 -0400

    add some links

--- gstream/app.py
+++ gstream/app.py
@@ -5,9 +5,13 @@ import datetime
 import requests, re, ssl, json
 import boto3
 from gstream.config import Config
+from dlx import DB as DLX
+from dlx.file import File as DLXFile
+from dlx.marc import BibSet, Query
 
 secrets = json.loads(Config.api_secrets)
 app = Flask(__name__)
+db_client = DLX.connect(Config.connect_string)
 
 class MetadataObject(object):
     def __init__(self, metadata):
@@ -22,6 +26,7 @@ class MetadataObject(object):
         self.distributionType = metadata['distributionType']
         self.title = metadata['title']
         self.files = []
+        self.links = []
 
     def __str__(self):
         return str({
@@ -77,6 +82,19 @@ def index():
             symbol_objects[m.symbol1] = m
         else:
             symbol_objects[m.symbol1].files.append(f)
-        
+
+    for s in symbol_objects:
+        returned_files = DLXFile.find({'identifiers': [{'type': 'symbol', 'value': s}], 'languages': ['EN']})
+        for f in returned_files:
+            symbol_objects[s].links.append(('PDF', f"https://{f.uri}"))
+
+        query = Query.from_string(f'191__a:{s}')
+        res = list(BibSet.from_query(query.compile()))
+        try:
+            my_s = res[0].get_value('191','a')
+            if my_s == s:
+                symbol_objects[s].links.append(('UNDL', f"{Config.dlx_endpoint}records/bibs/{res[0].id}"))
+        except:
+            pass
 
     return render_template('index.html', duty_stations=Config.duty_stations, data=symbol_objects, date=date, duty_station=duty_station, next_date=next_date, prev_date=prev_date)
--- gstream/config.py
+++ gstream/config.py
@@ -3,6 +3,8 @@ import boto3
 class Config(object):
     client = boto3.client('ssm')
     api_secrets = client.get_parameter(Name='gdoc-api-secrets')['Parameter']['Value']
+    connect_string = client.get_parameter(Name='connect-string')['Parameter']['Value']
+    dlx_endpoint = 'https://td1ljyw4rd.execute-api.us-east-1.amazonaws.com/prod/'
     duty_stations = [
         ('New York', 'NY'),
         ('Geneva', 'GE')
