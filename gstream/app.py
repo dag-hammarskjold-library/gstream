d71b0cbf4a94f78637de58b8e973e2a80178ae9a
Aaron Helton <helton@un.org>
2023-06-30 09:38:10 -0400

    update connect string and add database name

--- gstream/app.py
+++ gstream/app.py
@@ -11,7 +11,7 @@ from dlx.marc import BibSet, Query
 
 secrets = json.loads(Config.api_secrets)
 app = Flask(__name__)
-db_client = DLX.connect(Config.connect_string)
+db_client = DLX.connect(Config.connect_string, database=Config.dbname)
 
 class MetadataObject(object):
     def __init__(self, metadata):
